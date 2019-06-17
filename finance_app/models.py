cb42061275ca1c260b2b80b0f76520c1c1f53103
Jacob Schmidt <schhmidt.jacob3@gmail.com>
2019-06-17 15:17:25 -0400

    6/17/19

--- finance_app/models.py
+++ finance_app/models.py
@@ -4,11 +4,13 @@ from django.db import models
 class Item(models.Model):
     asset_name = models.CharField(max_length=30)
     asset_class = models.CharField(max_length=30)
-    quantity = models.CharField(max_length=30)
-    price = models.CharField(max_length=30)
+    quantity = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField( max_digits=10, decimal_places=2)
     sold_assets = models.BooleanField(default = False)
 
     # def __str__(self):
     #     return self.asset_name
-
+class PlotItem(models.Model):
+    x_value = models.DecimalField(max_digits=30, decimal_places=2)
+    y_value = models.DecimalField(max_digits=30, decimal_places=2)
 
--- finance_app/views.py
+++ finance_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Item 
+from .models import Item, PlotItem
+
 
 def index(request):
     return render(request, 'finance_app/home.html')
@@ -29,8 +30,8 @@ def buy_asset(request):
         new_asset = Item()
         new_asset.asset_name=request.POST.get('asset_name')
         new_asset.asset_class=request.POST.get('asset_class')
-        new_asset.price=request.POST.get('price')
-        new_asset.quantity=request.POST.get('quantity')
+        new_asset.price=int(request.POST.get('price'))
+        new_asset.quantity=int(request.POST.get('quantity'))
         new_asset.save()
 
         return redirect('portfolio')
@@ -44,7 +45,7 @@ def sold(request):
         #to_sell = Item.object.get(id=request.POST['sold_assets'])
         item = Item.objects.get(id=request.POST.get('id'))
 
-        item.sold_assets = False
+        item.sold_assets = True
 
         item.save()
 
@@ -62,4 +63,17 @@ def delete(request):
         return redirect('portfolio')
     
     return redirect('portfolio')
+
+# make another page for inputing data, this then is plugged into a function that generates and prints a graph
+# make a login page that uses boolean to bring up database
+
+def create_plot(request):
     
+    if request.method == 'POST':
+        
+        plot_items = PlotItem()
+        plot_item.x_value = request.POST.get('x_value')
+        plot_item.y_value = request.POST.get('y_value')
+        plot_items.save()
+        
+        
